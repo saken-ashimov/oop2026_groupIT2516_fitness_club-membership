9ca2cae53120114f13a028ba94a1ff484c8d5409
Saken <sakenashimov2007@gmail.com>
2026-01-17 11:51:19 +0500

    Saken Ashimov - Add Database

--- scr/Database/IDB.java
+++ scr/Database/IDB.java
@@ -1,4 +1,9 @@
 package Database;
 
-public class IDB {
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public interface IDB {
+    // Метод, который должен вернуть активное соединение
+    Connection getConnection() throws SQLException, ClassNotFoundException;
 }
--- scr/Database/PostgresDB.java
+++ scr/Database/PostgresDB.java
@@ -1,4 +1,26 @@
 package Database;
 
-public class PostgresDB {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class PostgresDB implements IDB {
+    private String username = "postgres.dfngjfjxtcauwibocaga"; // твой логин (обычно postgres)
+    private String password = "SEHCNI9_CBBreggiN";     // ТВОЙ ПАРОЛЬ
+
+    @Override
+    public Connection getConnection() throws SQLException, ClassNotFoundException {
+        String connectionUrl = "jdbc:postgresql://aws-1-ap-south-1.pooler.supabase.com:5432/postgres?sslmode=require";
+
+        try {
+            Class.forName("org.postgresql.Driver");
+            Connection connection = DriverManager.getConnection(connectionUrl, username, password);
+
+            return connection;
+        } catch (Exception e) {
+            System.out.println("Failed to connect to database!");
+            e.printStackTrace();
+            throw e; //
+        }
+    }
 }
