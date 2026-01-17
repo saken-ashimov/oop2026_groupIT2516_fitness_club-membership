6d5ec0d61e01d8d64484c28b30af8511c2d3700e
Saken <sakenashimov2007@gmail.com>
2026-01-17 12:46:41 +0500

    Saken Ashimov - hide password

--- scr/Database/PostgresDB.java
+++ scr/Database/PostgresDB.java
@@ -3,10 +3,27 @@ package Database;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 
 public class PostgresDB implements IDB {
     private String username = "postgres.dfngjfjxtcauwibocaga"; // твой логин (обычно postgres)
-    private String password = "SEHCNI9_CBBreggiN";     // ТВОЙ ПАРОЛЬ
+    private String password =  loadPassword();     // ТВОЙ ПАРОЛЬ
+    private static String loadPassword() {
+        Properties props = new Properties();
+        try (InputStream input = new FileInputStream("config.properties")) {
+            props.load(input);
+            String value = props.getProperty("DB_PASSWORD");
+            if (value == null || value.isBlank()) {
+                throw new RuntimeException("DB_PASSWORD is not set in config.properties");
+            }
+            return value;
+        } catch (IOException e) {
+            throw new RuntimeException("Cannot load DB_PASSWORD from config.properties", e);
+        }
+    }
 
     @Override
     public Connection getConnection() throws SQLException, ClassNotFoundException {
